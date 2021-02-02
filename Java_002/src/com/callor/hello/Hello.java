a0339692f9e9ef016b0e89ae16c9242ae7254894
pinaoguy-jo <pianoguy@nate.com>
2021-02-02 09:45:58 +0900

    java_003 연습

--- Java_002/src/com/callor/hello/Hello.java
+++ Java_002/src/com/callor/hello/Hello.java
@@ -1,7 +1,7 @@
 package com.callor.hello;
 
 public class Hello {
-	public static void main(String[] gras) {
+	public static void main(String[] args) {
 		System.out.println("30 + 40 = " + (30 + 40));
 		System.out.println("30 * 40 = " + (30 * 40));
 		System.out.println("40 - 30 = " + (40 - 30));
---
+++ Java_003/src/com/callor/hello/Hello.java
@@ -0,0 +1,24 @@
+package com.callor.hello;
+
+public class Hello {
+	/*
+	 * main() method 라고 부른다.
+	 * Run(eclipse)를 했을때 꼭 필요한 코드
+	 * 
+	 * eclipse에서 Run을 했을 때 발생하는 사항
+	 * Hello.java 파일을 컴파일 :java Hello.java
+	 * Hello.class 파일이 생성되면 java Hello 명령을 실행하여
+	 * 코드를 컴퓨터에 주입하고 결과를 Console에 출력
+	 */
+	
+	public static void main(String[] args){	
+		
+		System.out.println("30+40 = " +(30+40));
+		System.out.println("30 * 40 = " + 30*40);
+		System.out.println("40 - 30 = " + (40 - 30));
+		System.out.println("40 / 20 = " + 40/20);
+		System.out.println("반갑습니다");
+	}
+	
+	
+}
