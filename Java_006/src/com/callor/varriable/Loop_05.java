cb00f27e60c11ae3372a098922203bdc800bb63c
pianoguy <pianoguy@nate.com>
2021-02-04 16:33:53 +0900

    반복문

---
+++ Java_006/src/com/callor/varriable/Loop_05.java
@@ -0,0 +1,25 @@
+package com.callor.varriable;
+
+public class Loop_05 {
+	public static void main(String arge[]) {
+		
+		int intDan = 7;
+		for(int num = 1; num < 10 ; num += 1) {
+			
+		}
+		for(int num = 1; num < 10 ; num += 1) {
+			
+		}
+		for(int num =20 ; num < 42 ; num += 1) {
+			System.out.println(num + "안녕");				
+		}
+		//  시작하는 값 ;/비교하는 값/증가하는 값
+		for(int num =12 ; num <= 20 ; num += 1) {
+			System.out.println(num + "반가워");
+		}
+		for(int num = 12 ; num <=20 ; num += 2) {
+			System.out.println(num);
+		}
+	}
+
+}
