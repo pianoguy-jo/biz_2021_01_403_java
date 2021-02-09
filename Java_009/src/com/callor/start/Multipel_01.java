276a02839b64686c5fae368ea95b719c9c292844
pianoguy <pianoguy@nate.com>
2021-02-09 09:42:18 +0900

    3과 9의 배수의 합

---
+++ Java_009/src/com/callor/start/Multipel_01.java
@@ -0,0 +1,58 @@
+package com.callor.start;
+
+public class Multipel_01 {
+
+	public static void main(String arge[]) {
+		/*
+		int var = 0; 
+		
+		for(int i =0 ; i < 100 ; i++) {
+			 
+			
+			boolean bcr = i % 3 == 0 && i % 9== 0;
+			
+			if(bcr) {
+				
+				System.out.println(i);
+				var += i;
+				
+			}
+		}
+		System.out.println(var);
+		*/
+		
+		
+		// 1 ~ 100 까지 범위에서 찾아
+		for(int i =0 ; i < 100 ; i++) {
+			
+			int intSum = i +1;
+			
+			boolean bYes3M = i % 3 == 0;
+			boolean bYes9M = i % 9 == 0;
+			
+			// 3의 배수이면서(and) 9의 배수
+			if(bYes3M && bYes9M) {
+				intSum +=1; // i를 더하지 않는다.
+				System.out.println(intSum + "는 3과 9의 배수")
+			}			
+		}
+		System.out.println("3과 9의 배수합 : " + intSum);
+		System.out.println("=======================")
+		
+for(int i =0 ; i < 100 ; i++) {
+			
+			int num1 = i +1;
+			
+			boolean bYes3M = i % 3 == 0;
+			boolean bYes9M = i % 9 == 0;
+			
+			// if() 비교연산문을 중복 처리하는 방식
+			if(bYes3M) {
+				if(bYes9M) {
+					intSum +=1;
+					System.out.println(num1);
+				}
+			}
+			System.out.println("3과 9의 배수합 : " + intSum);
+	}
+}
